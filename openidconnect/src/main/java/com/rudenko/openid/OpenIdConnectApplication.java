39deba4342fb457bea5a015ff38903c617a019b6
Mithilesh SRavindran <mravindran@mobiquityinc.com>
2018-06-08 13:19:18 +0200

    Code changes for the Google openid connect

---
+++ openidconnect/src/main/java/com/rudenko/openid/OpenIdConnectApplication.java
@@ -0,0 +1,12 @@
+package com.rudenko.openid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenIdConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenIdConnectApplication.class, args);
+	}
+}
